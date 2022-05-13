06727bdf8b0574e69e6f51afdf320ccad75128e4
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2022-05-13 17:13:22 -0300

    fetch - funciones asincronas

---
+++ fetch_funcion_asincrona/main.js
@@ -0,0 +1,12 @@
+async function obtener_datos(){
+
+    //fetch realiza una solicitud HTTP a traves del metodo GET
+    const datos = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    const datos_definitivos = await datos.json()
+
+    datos_definitivos.forEach(element => {
+        console.log(element.title)
+    });
+
+}
