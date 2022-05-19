664e168f5961a39db3a4e0033579b7cc544d566c
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2022-05-19 17:04:30 -0300

    fetch tabla y usuarios

--- fetch_funcion_asincrona/main.js
+++ fetch_funcion_asincrona/main.js
@@ -1,6 +1,8 @@
 async function obtener_datos(){
 
     //fetch realiza una solicitud HTTP a traves del metodo GET
+    // endpoint o ruta
+
     const datos = await fetch('https://jsonplaceholder.typicode.com/posts')
 
     const datos_definitivos = await datos.json()
