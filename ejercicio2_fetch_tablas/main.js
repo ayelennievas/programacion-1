24427fe91371d6f303882868c7530212a2672ded
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2022-05-27 17:02:38 -0300

    ejercicio2_fetch_catalogo

--- ejercicio2_fetch_tablas/main.js
+++ ejercicio2_fetch_tablas/main.js
@@ -1,19 +1,45 @@
-const obtener_productos = async () => {
+const obtener = async (categoria) => {
 
-    const producto = await fetch ('https://fakestoreapi.com/products/categories'+ categoria)
+    const producto = await fetch ('https://fakestoreapi.com/products/category/' + categoria)
  
-    const obtener_productos = await producto.json()
+    const productos_seleccionados = await producto.json()
 
-    let filas = []
+    let columnas = []
 
-    obtener_productos.forEach(element => {
+    productos_seleccionados.forEach(element => {
+
+        let columna =
         
-        `<div class="card-body">
+    `<div class="col-lg-3">
+        <div class="card">
+          <img src="${element.image}" class="card-img-top" alt="...">
+          <div class="card-body">
             <h5 class="card-title">${element.title}</h5>
-            <p class="card-text">${element.description}</p>
-            <p class="card-text">${element.price}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>`
+
+            <p>
+            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample_${element.id}" aria-expanded="false" aria-controls="collapseExample">
+            Descripcion
+            </button>
+            </p>
+
+            <div class="collapse" id="collapseExample_${element.id}">
+                <div class="card card-body">
+                 <p class="card-text">${element.description}</p>
+                </div>
+            </div>
+
+            <p class="card-text">$${element.price}</p>
+            <a href="#" class="btn btn-primary">Comprar</a>
+          </div>
+
+        </div>
+    </div>`
+
+      columnas.push(columna)
+
     });
 
+    document.getElementById("catalogo").innerHTML = columnas.join('')
+
 }
+
