c8e6769565d27f57267e9f89211095886bef12d3
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2022-06-16 17:00:09 -0300

    repaso_tp2api

---
+++ repaso_tp2api/main.js
@@ -0,0 +1,21 @@
+const obtener = async () => {
+
+    const provincias = await fetch ('https://apis.datos.gob.ar/georef/api/provincias')
+ 
+    const provincias_seleccionadas = await provincias.json()
+
+    let filas = []
+
+    provincias_seleccionadas.forEach((element,index) => {
+        
+        let fila =
+        `<tr>
+           <td>${index+1}</td>
+           <td>${element.nombre}</td>
+        </tr>`
+
+        filas.push(fila)
+    });
+
+     document.getElementById("tbl_body").innerHTML = filas.join('')
+}
