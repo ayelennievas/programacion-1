9a51bfa8d889cc2929eaf9b8e69dde0a703fa989
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2022-06-23 17:10:11 -0300

    estructuras_repetitivas

---
+++ actividad recorrer arreglos/main.js
@@ -0,0 +1,25 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+    numeros.forEach((element) => {
+        if((element+1)%2){
+            console.log(element)
+        }
+        
+        
+    });
+
+    for (const iterator of numeros) {
+        if(iterator>2 && iterator<9){
+            console.log(iterator)
+        }
+        
+    }
+
+    for (const key in numeros) {
+        
+        
+    }
+
+
