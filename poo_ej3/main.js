3642662936fb7bfcf5cd58b04d90179306fb93a2
ayelennievas <tamara.ayelen.nievas05@gmail.com>
2022-06-30 17:03:01 -0300

    poo ej3

---
+++ poo_ej3/main.js
@@ -0,0 +1,25 @@
+import Producto from "./producto.js";
+
+function guardar(){
+
+    let descripcion = document.getElementById("inp_descripcion").value
+    let precio_venta = document.getElementById("inp_precio_venta").value
+    let categoria = document.getElementById("slt_cat").value
+
+    let producto = new Producto(descripcion,precio_venta,categoria)
+
+    //invocamos(ejecutamos) al metodo guardar_producto()
+    //perteneciente a la clase Producto
+    producto.guardar_producto()
+}
+
+document.getElementById("btn_guardar").addEventListener("click",guardar)
+
+function listar(){
+
+    let producto = new Producto()
+    producto.obtener_productos()
+    
+}
+
+listar()
